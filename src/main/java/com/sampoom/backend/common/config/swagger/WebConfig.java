7bacddf8c16f6bfe6806219f9837c7be38e111b2
Kim Taemin <ruhokim1@naver.com>
2025-09-30 16:29:32 +0900

    [SETTING] CI 설정

--- src/main/java/com/sampoom/backend/common/config/swagger/WebConfig.java
+++ src/main/java/com/sampoom/backend/common/config/swagger/WebConfig.java
@@ -12,7 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로 허용
                 .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("http://0.0.0.0:5200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true) // 쿠키 허용 시 필요
