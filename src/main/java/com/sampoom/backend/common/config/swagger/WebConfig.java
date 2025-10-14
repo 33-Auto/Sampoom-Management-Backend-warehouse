3da9522162c47ce9f328d91f70e7fd8f0f94b8da
Admin <admin@Adminui-MacBookPro-7.local>
2025-10-14 17:03:32 +0900

    fix: cors cors origin 백엔드 도메인으로 수정

--- src/main/java/com/sampoom/backend/common/config/swagger/WebConfig.java
+++ src/main/java/com/sampoom/backend/common/config/swagger/WebConfig.java
@@ -11,11 +11,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로 허용
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://sampoom.store")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true) // 쿠키 허용 시 필요
                 .maxAge(3600); // preflight 캐싱 시간 (초)
     }
-
 }
