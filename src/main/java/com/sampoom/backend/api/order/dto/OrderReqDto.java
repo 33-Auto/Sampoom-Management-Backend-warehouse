b6a7d19d165a20af497a9e8d99020d811f1ed31f
Admin <admin@Adminui-MacBookPro-7.local>
2025-10-20 02:36:27 +0900

    fix: 요청 디티오 검증

--- src/main/java/com/sampoom/backend/api/order/dto/OrderReqDto.java
+++ src/main/java/com/sampoom/backend/api/order/dto/OrderReqDto.java
@@ -1,5 +1,8 @@
 package com.sampoom.backend.api.order.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,6 +15,9 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderReqDto {
+    @NotBlank(message = "주문 지점 정보는 필수입니다")
     private String branch;
+    @NotEmpty(message = "주문 항목은 최소 1개 이상이어야 합니다")
+    @Valid
     private List<ItemDto> items;
 }
