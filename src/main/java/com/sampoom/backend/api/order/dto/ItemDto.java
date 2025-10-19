1720a2e012088a5c8f29212eed7f4ff1cda1d0b1
Admin <admin@Adminui-MacBookPro-7.local>
2025-10-20 02:35:23 +0900

    fix: 요청 디티오 검증 아이템 검증

--- src/main/java/com/sampoom/backend/api/order/dto/ItemDto.java
+++ src/main/java/com/sampoom/backend/api/order/dto/ItemDto.java
@@ -1,5 +1,8 @@
 package com.sampoom.backend.api.order.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +13,9 @@ import lombok.ToString;
 @ToString
 @Builder
 public class ItemDto {
+    @NotBlank(message = "부품 코드는 필수입니다")
     private String code;
+    @NotNull(message = "수량은 필수입니다")
+    @Min(value = 1, message = "수량은 1 이상이어야 합니다")
     private Integer quantity;
 }
