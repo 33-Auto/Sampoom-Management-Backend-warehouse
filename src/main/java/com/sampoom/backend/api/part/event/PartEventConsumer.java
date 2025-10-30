e9e549df59b40ba37f68d6083bbe0b8f3b0c4e3e
Admin <admin@Adminui-MacBookPro-7.local>
2025-10-30 18:03:51 +0900

    refac: part event consumer 컨수머 핸들러 만들고 서비스 로직으로 분리

--- src/main/java/com/sampoom/backend/api/part/event/PartEventConsumer.java
+++ src/main/java/com/sampoom/backend/api/part/event/PartEventConsumer.java
@@ -3,10 +3,16 @@ package com.sampoom.backend.api.part.event;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sampoom.backend.api.part.dto.Event;
+import com.sampoom.backend.api.part.dto.PartCategoryPayload;
+import com.sampoom.backend.api.part.dto.PartGroupPayload;
 import com.sampoom.backend.api.part.dto.PartPayload;
+import com.sampoom.backend.api.part.entity.Category;
 import com.sampoom.backend.api.part.entity.Part;
 import com.sampoom.backend.api.part.entity.PartGroup;
+import com.sampoom.backend.api.part.repository.CategoryRepository;
+import com.sampoom.backend.api.part.repository.PartGroupRepository;
 import com.sampoom.backend.api.part.repository.PartRepository;
+import com.sampoom.backend.api.part.service.PartService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.codec.multipart.PartEvent;
@@ -17,9 +23,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Slf4j
 public class PartEventConsumer {
-    private final PartRepository partRepository;
     private final ObjectMapper objectMapper;
     private final EventPayloadMapper eventPayloadMapper;
+    private final PartService partService;
 
     @KafkaListener(topics = "part-events")
     public void consume(String message) {
@@ -34,25 +40,21 @@ public class PartEventConsumer {
 
             if ("PartCreated".equals(eventType)) {
                 PartPayload payload = (PartPayload) event.getPayload();
-
-                Part part = Part.builder()
-                        .id(payload.getPartId())
-                        .groupId(payload.getGroupId())
-                        .categoryId(payload.getCategoryId())
-                        .name(payload.getName())
-                        .code(payload.getCode())
-                        .isDeleted(payload.getDeleted())
-                        .status(payload.getStatus())
-                        .unit(payload.getPartUnit())
-                        .safetyStock(payload.getBaseQuantity())
-                        .build();
-
-                partRepository.save(part);
+                partService.createPart(payload);
                 log.info("✅ partCreated saved: {}", payload.getName());
+            }
+            else if ("PartGroupCreated".equals(eventType)) {
+                PartGroupPayload payload = (PartGroupPayload) event.getPayload();
+                partService.createPartGroup(payload);
+                log.info("✅ partGroupCreated saved: {}", payload.getGroupName());
+            } else if ("PartCategoryCreated".equals(eventType)) {
+                PartCategoryPayload payload = (PartCategoryPayload) event.getPayload();
+                partService.createPartCategory(payload);
+                log.info("✅ PartCategoryCreated saved: {}", payload.getCategoryName());
             }
 
         } catch (Exception e) {
-            log.error("❌ Failed to process PartCreated event", e);
+            log.error("❌ Failed to process part event", e);
         }
     }
 }
---
+++ src/main/java/com/sampoom/backend/api/part/service/PartService.java
@@ -0,0 +1,58 @@
+package com.sampoom.backend.api.part.service;
+
+import com.sampoom.backend.api.part.dto.PartCategoryPayload;
+import com.sampoom.backend.api.part.dto.PartGroupPayload;
+import com.sampoom.backend.api.part.dto.PartPayload;
+import com.sampoom.backend.api.part.entity.Category;
+import com.sampoom.backend.api.part.entity.Part;
+import com.sampoom.backend.api.part.entity.PartGroup;
+import com.sampoom.backend.api.part.repository.CategoryRepository;
+import com.sampoom.backend.api.part.repository.PartGroupRepository;
+import com.sampoom.backend.api.part.repository.PartRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PartService {
+    private final PartRepository partRepository;
+    private final PartGroupRepository partGroupRepository;
+    private final CategoryRepository categoryRepository;
+
+    public void createPartCategory(PartCategoryPayload payload) {
+        Category category = Category.builder()
+                .id(payload.getCategoryId())
+                .name(payload.getCategoryName())
+                .code(payload.getCategoryCode())
+                .build();
+
+        categoryRepository.save(category);
+    }
+
+    public void createPartGroup(PartGroupPayload payload) {
+        PartGroup group = PartGroup.builder()
+                .id(payload.getGroupId())
+                .categoryId(payload.getCategoryId())
+                .name(payload.getGroupName())
+                .code(payload.getGroupCode())
+                .build();
+
+        partGroupRepository.save(group);
+    }
+
+    public void createPart(PartPayload payload) {
+        Part part = Part.builder()
+                .id(payload.getPartId())
+                .groupId(payload.getGroupId())
+                .categoryId(payload.getCategoryId())
+                .name(payload.getName())
+                .code(payload.getCode())
+                .isDeleted(payload.getDeleted())
+                .status(payload.getStatus())
+                .unit(payload.getPartUnit())
+                .safetyStock(payload.getBaseQuantity())
+                .build();
+
+        partRepository.save(part);
+    }
+}
